cb87d74b14048d355080619c46586a744bc8ec63
DEXTER <dexterid2003@gmail.com>
2025-01-01 02:26:11 +0530

    Add config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+const config = {
+    telegramBotToken: "7789638576:AAFkkoIxrjAWBZiNBuz9xyaKQouHcDDRhfA", // Replace with your Telegram Bot Token DEXTER ID 📍
+    chatId: "6723222778" // Replace with your Telegram Chat ID DEXTER ID 📍
+};
+
+export default config;
